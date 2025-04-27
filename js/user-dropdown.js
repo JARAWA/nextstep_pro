74eb7713e5d7fba10f3c086d19b80a24a90349a5
JARAWA <129917912+JARAWA@users.noreply.github.com>
2025-04-28 01:24:34 +0530

    Update user-dropdown.js

--- js/user-dropdown.js
+++ js/user-dropdown.js
@@ -12,9 +12,9 @@ import {
     getDocs
 } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
-import { db } from './js/auth/services/firebase-config.js';
-import { ErrorHandler } from './js/auth/utils/error-handler.js';
-import { TokenManager } from './js/auth/services/token-manager.js';
+import { db } from './auth/services/firebase-config.js';
+import { ErrorHandler } from './auth/utils/error-handler.js';
+import { TokenManager } from './auth/services/token-manager.js';
 
 class UserService {
     static userData = null;
